6a95ef55a44eeac6d8d8f237651c7cab137639a8
albraa-A <albraa2999@gmail.com>
2026-02-24 22:28:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import replicate
 import os
 
 app = Flask(__name__)
+
+CORS(app)
 
 REPLICATE_API_TOKEN = os.environ.get("REPLICATE_API_TOKEN")
 
